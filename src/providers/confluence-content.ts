800750fabec6c25f3ec362bc0630d3c42599b3d8
Priyansh Jain <priyansh.j@endurance.com>
2021-04-17 21:45:13 +0530

    fix conflicts

--- src/providers/confluence-content.ts
+++ src/providers/confluence-content.ts
@@ -1,12 +1,23 @@
 import { ExtensionContext } from "vscode";
 import { Cache } from "../common/persistent-cache";
 import { ConfluenceSingleton } from "../common/confluence-singleton";
+import * as vscode from "vscode";
+import { HTMLElement, parse } from "node-html-parser";
+import { Constants } from "../common/constants";
+import { DocumentViewProvider } from "../DocumentViewProvider";
+import * as FileType from "file-type";
 
 export class ConfluenceContentProvider {
   private _cache: Cache;
+  private _imageStorageDirectory: vscode.Uri;
 
-  constructor(private readonly _context: ExtensionContext, cacheSize: number) {
+  constructor(
+    private readonly _context: ExtensionContext,
+    cacheSize: number,
+    storageDirectory: vscode.Uri
+  ) {
     this._cache = new Cache("page-cache", this._context, cacheSize);
+    this._imageStorageDirectory = storageDirectory;
   }
 
   public getCachedBodyViewById = async (id: any) => {
@@ -24,7 +35,7 @@ export class ConfluenceContentProvider {
       await ConfluenceSingleton.getConfluenceObject(this._context)
     ).getCustomContentById({
       id,
-      expanders: ["body.view", "space"],
+      expanders: ["body.view"],
     });
     return this._getDetailsFromResponse(response);
   };
@@ -33,11 +44,61 @@ export class ConfluenceContentProvider {
     this._cache.clearCache();
   };
 
-  private _getDetailsFromResponse = (response: any) => {
-    const html = escape(response["body"]["view"]["value"]);
+  private _getDetailsFromResponse = async (response: any) => {
+    const html = escape(
+      await this._storeImagesAndGetHtml(response["body"]["view"]["value"])
+    );
     const baseUrl = response["_links"]["base"];
     const pageUrl = response["_links"]["webui"];
     const title = response["title"];
     return { html, baseUrl, pageUrl, title };
+  };
+
+  private _storeImagesAndGetHtml = async (html: any): Promise<string> => {
+    let htmlParsed: HTMLElement = parse(html);
+    DocumentViewProvider.createOrShow(
+      this._context.extensionUri,
+      this._imageStorageDirectory
+    );
+    await Promise.all(
+      htmlParsed.querySelectorAll("img").map(async (image) => {
+        let imgUrl: string | undefined = image.getAttribute("src");
+        if (imgUrl === undefined) {
+          return;
+        }
+        const confluence = await ConfluenceSingleton.getConfluenceObject(
+          this._context
+        );
+        const imgResponse = await confluence.fetch(
+          Constants.baseUri + imgUrl,
+          "GET",
+          false
+        );
+        const fileType = await FileType.fromBuffer(imgResponse);
+        if (fileType === undefined) {
+          console.log(
+            "File type could not be reliably determined! The binary data may be malformed! No file saved!"
+          );
+          return;
+        }
+        const imgPath = vscode.Uri.joinPath(
+          this._context.globalStorageUri,
+          "images",
+          image.getAttribute("data-linked-resource-default-alias") || "sample"
+        );
+        await vscode.workspace.fs.writeFile(imgPath, imgResponse);
+        const webviewUri = DocumentViewProvider.currentPanel?._panel.webview.asWebviewUri(
+          imgPath
+        );
+        if (webviewUri === undefined) {
+          return;
+        }
+        image.setAttribute("src", webviewUri.toString());
+        image.setAttribute("data-image-src", webviewUri.toString());
+        image.removeAttribute("data-base-url");
+        return image;
+      })
+    );
+    return htmlParsed.toString();
   };
 }
